248f25d821a2ff6f10a0624d0ff721bd657eab0c
Sunmade1 <las4made@gmail.com>
2023-07-06 16:49:52 +0100

    First Java Project

---
+++ My-First-Java-Project/src/com/Ismail.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Ismail {
+    public static void main(String[] args) {
+        System.out.println("Ismail created his first java project successfully, thanks to Agape");
+    }
+}
